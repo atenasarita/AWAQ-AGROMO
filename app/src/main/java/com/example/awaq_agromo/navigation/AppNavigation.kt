4396585bc78a771be2f380292c924f76305a6156
Giosmaju <71265812+Giosmaju@users.noreply.github.com>
2025-10-14 18:32:36 -0600

    Update AppNavigation.kt

--- app/src/main/java/com/example/awaq_agromo/navigation/AppNavigation.kt
+++ app/src/main/java/com/example/awaq_agromo/navigation/AppNavigation.kt
@@ -5,6 +5,8 @@ import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import com.example.awaq_agromo.presentation.screens.camera.PhotoScreen
+import com.example.awaq_agromo.presentation.screens.diagnostico.DiagnosticoScreen
+import com.example.awaq_agromo.presentation.screens.finalizacion.RegistroExitosoScreen
 import com.example.awaq_agromo.presentation.screens.login.LoginScreen
 import com.example.awaq_agromo.presentation.screens.onboarding.Onboarding_Page_1Screen
 import com.example.awaq_agromo.presentation.screens.onboarding.Onboarding_Page_2Screen
@@ -12,6 +14,8 @@ import com.example.awaq_agromo.presentation.screens.onboarding.Onboarding_Page_3
 import com.example.awaq_agromo.presentation.screens.perfil.PerfilScreen
 import com.example.awaq_agromo.presentation.screens.registration.RegistrationScreen
 import com.example.awaq_agromo.presentation.screens.welcome.WelcomeScreen
+import com.example.awaq_agromo.presentation.screens.manejo_aplicado.FertilizacionScreen
+
 
 @Composable
 fun AppNavigation(navController: NavHostController) {
@@ -19,53 +23,72 @@ fun AppNavigation(navController: NavHostController) {
 
         composable("welcome") {
             WelcomeScreen(
-                onRegisterClick = {
-                    navController.navigate("registration")
-                },
-                onLoginClick = {
-                    navController.navigate("login")
-                }
+                onRegisterClick = { navController.navigate("registration") },
+                onLoginClick = { navController.navigate("login") }
             )
         }
+
         composable("login") {
             LoginScreen(onDashboardClick = { navController.navigate("dashboard") })
         }
+
         composable("registration") {
-            RegistrationScreen(
-                onOnboardingClick = { navController.navigate("Onboarding_1") }
-            )
+            RegistrationScreen(onOnboardingClick = { navController.navigate("Onboarding_1") })
         }
+
         composable("Onboarding_1") {
             Onboarding_Page_1Screen(
-                onNextOnboardingPage2 = {navController.navigate("Onboarding_2")}
+                onNextOnboardingPage2 = { navController.navigate("Onboarding_2") }
             )
         }
+
         composable("Onboarding_2") {
             Onboarding_Page_2Screen(
-                onNextOnboardingPage3 = {navController.navigate("Onboarding_3")}
+                onNextOnboardingPage3 = { navController.navigate("Onboarding_3") }
             )
         }
+
         composable("Onboarding_3") {
             Onboarding_Page_3Screen(
-                onDashboardClick = {navController.navigate("dashboard")}
+                onDashboardClick = { navController.navigate("dashboard") }
             )
         }
+
         composable("dashboard") {
             /*Dashboard(
-                onPerfilClick = {navController.navigate("perfil")}
+                onPerfilClick = { navController.navigate("perfil") }
             )*/
         }
+
         composable("perfil") {
             PerfilScreen()
         }
+
         composable("cultivo") {
             IdCultivoScreen(navController)
         }
+
         composable("clima") {
             ClimaScreen(navController)
         }
+
         composable("photo") {
             PhotoScreen(navController)
+        }
+
+
+        composable("fertilizacion") {
+            FertilizacionScreen(navController)
+        }
+
+
+        composable("diagnostico") {
+            DiagnosticoScreen(navController)
+        }
+
+
+        composable("registro_exitoso") {
+            RegistroExitosoScreen(navController)
         }
     }
 }
